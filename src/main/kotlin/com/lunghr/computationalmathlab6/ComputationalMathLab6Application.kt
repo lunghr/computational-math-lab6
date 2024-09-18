69555633b311f1aa34b3d605cbcf7bb9de041faf
lunghr <myilanka8@gmail.com>
2024-09-18 20:01:12 +0300

    main

---
+++ src/main/kotlin/com/lunghr/computationalmathlab6/ComputationalMathLab6Application.kt
@@ -0,0 +1,11 @@
+package com.lunghr.computationalmathlab6
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComputationalMathLab6Application
+
+fun main(args: Array<String>) {
+    runApplication<ComputationalMathLab6Application>(*args)
+}
---
+++ src/test/kotlin/com/lunghr/computationalmathlab6/ComputationalMathLab6ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lunghr.computationalmathlab6
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ComputationalMathLab6ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
